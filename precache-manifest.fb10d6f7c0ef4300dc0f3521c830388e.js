768cd33807280fd8cec2c28e33807a26abae2f39
gtrakas1 <gtrakas23@gmail.com>
2020-07-13 10:42:47 -0500

    Updates

---
+++ precache-manifest.fb10d6f7c0ef4300dc0f3521c830388e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772970aa817ea38ca67f4c9c97c70123",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "051d57aa896cf3bfb090",
+    "url": "/React/static/css/main.d2af9a0b.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/React/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "051d57aa896cf3bfb090",
+    "url": "/React/static/js/main.a2cf3479.chunk.js"
+  },
+  {
+    "revision": "07f365e02c7d8274105b",
+    "url": "/React/static/js/runtime-main.6c2acf4b.js"
+  }
+]);
